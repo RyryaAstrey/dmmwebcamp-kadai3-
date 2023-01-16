d60aec8c89bb860db0def79d3fdda7c396684e4b
Ryrya Astrey <119737958+RyryaAstrey@users.noreply.github.com>
2023-01-16 07:50:19 +0000

    ログイン後の変移先の設定、フラッシュメッセージの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about', as: "about"
   resource :users, only: [:index, :show, :edit]
-  resource :books , only: [:index, :show, :edit]
+  resource :books, only: [:index, :show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
